6c16baf8bf7dd3e0f3062adb03def06c09b5e642
Vincent BARON <vbaron@student.42.fr>
2020-06-22 17:55:33 +0200

    Libft with modified split added

--- srcs/map_parsing.c
+++ srcs/map_parsing.c
@@ -6,7 +6,7 @@
 /*   By: vbaron <vbaron@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/06/19 12:35:31 by vbaron            #+#    #+#             */
-/*   Updated: 2020/06/22 17:36:37 by vbaron           ###   ########.fr       */
+/*   Updated: 2020/06/22 17:45:21 by vbaron           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,18 +15,10 @@
 
 int     args_definer(t_input *args, int x)
 {
-        t_args *args_tab[8];
         char *splitter;
         int f;
 
-        args_tab[0] = args->R;
-        args_tab[1] = args->N;
-        args_tab[2] = args->S;
-        args_tab[3] = args->WE;
-        args_tab[4] = args->EA;
-        args_tab[5] = args->S;
-        args_tab[6] = args->F;
-        args_tab[7] = args->C;
+        args_tab = initialize_tab(args);
 
         while (args->line[f])
         {
