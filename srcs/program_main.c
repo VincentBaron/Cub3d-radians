0e9ea8fc51bb8b53bd2850edd5d3faa25bf81082
Vincent Baron <vbaron@student.42.fr>
2020-09-28 10:23:00 +0200

    mlx_testing

--- srcs/program_main.c
+++ srcs/program_main.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   program_main.c                                     :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: vbaron <vbaron@student.42.fr>              +#+  +:+       +#+        */
+/*   By: vincentbaron <vincentbaron@student.42.f    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/06/19 12:38:34 by vbaron            #+#    #+#             */
-/*   Updated: 2020/06/24 16:49:04 by vbaron           ###   ########.fr       */
+/*   Updated: 2020/09/28 10:22:32 by vincentbaro      ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,6 +23,8 @@ int main(int argc, char **argv)
     
     //if (argc != 2)
       //  error();
+    (void)argc;
+    (void)argv;
     if ((mother.args.fd = open("test.txt", O_RDONLY)) == -1)
         error();
     map_parsing(&(mother.args));
