ac2a558546df5b1eba5a92a5131a3f4ede254192
Vincent BARON <vbaron@student.42.fr>
2020-10-19 10:10:55 +0200

    first try pixel movement

--- srcs/virtual_function.c
+++ srcs/virtual_function.c
@@ -6,7 +6,7 @@
 /*   By: vbaron <vbaron@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/06/24 16:49:29 by vbaron            #+#    #+#             */
-/*   Updated: 2020/10/19 10:08:36 by vbaron           ###   ########.fr       */
+/*   Updated: 2020/10/19 10:10:53 by vbaron           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -128,9 +128,9 @@ void redefine_map(int keycode, t_general *mother)
 
 int  new_map(int keycode, t_general *mother)
 {
+     redefine_map(keycode, mother);
      mother->mlx.img_map.image = mlx_new_image(mother->mlx.ptr, mother->args.R[0], mother->args.R[1]);
      mother->mlx.img_map.addr = mlx_get_data_addr(mother->mlx.img_map.image, &(mother->mlx.img_map.bpp), &(mother->mlx.img_map.size_line), &(mother->mlx.img_map.endian));
-     redefine_map(keycode, mother);
      draw_map(mother);
      mlx_put_image_to_window(mother->mlx.ptr, mother->mlx.win, mother->mlx.img_map.image, 0, 0);
      //mlx_destroy_image(mother->mlx.ptr, mother->mlx.img_map.image);
